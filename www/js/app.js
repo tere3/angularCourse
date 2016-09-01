864e848b5964e7ddf6366c5be9e61364f4cc5485
tere3 <teremendezr@hotmail.com>
2016-09-01 12:02:21 -0600

    Ng-init, -ng-show, tablas

--- www/js/app.js
+++ www/js/app.js
@@ -16,7 +16,8 @@
 		{id:0, url: 'img/gem-01.gif'}, //url estamos agregando el objeto de imagen
 		{id:1, url: 'img/gem-02.gif'},
 		{id:2, url: 'img/gem-03.gif'}
-		]
+		],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique cum magni est ratione nemo sed voluptatem, incidunt, quisquam repellat quas magnam aliquid animi facere cupiditate, molestiae! Facilis quasi alias eos.'
 		},
 		{name: 'Bloodstone', 
 		price: 5.95,
@@ -24,7 +25,8 @@
 		{id:0 ,url: 'img/gem-04.gif'}, 
 		{id:1 ,url: 'img/gem-05.gif'},
 		{id:2, url: 'img/gem-06.gif'}
-		]
+		],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus porro omnis aliquid, atque magnam non voluptates facilis officiis nam delectus beatae quod, illum ullam, unde quae. Quae et, vero corrupti!'
 		},
 		{name: 'Zircon', 
 		price: 3.95,
@@ -32,13 +34,26 @@
 		{id:0, url: 'img/gem-07.gif'}, 
 		{id:1, url: 'img/gem-08.gif'},
 		{id:2, url: 'img/gem-09.gif'}
-		]
+		],
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis id eligendi assumenda, cumque optio veniam eos perferendis molestias explicabo odit. Libero, suscipit. Quae praesentium tempore minima quod tempora odio cumque.'
 		},
 	]; //arreglo de objetos
 	var app = angular.module('gemStore', []); //modulo
 
 	app.controller('StoreController', function(){ //controlador del modulo
 		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){ //asiganmos valor ala variable que inicializamos
+			this.tab = setTab;
+		}
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		}
 	});
 })();
 
